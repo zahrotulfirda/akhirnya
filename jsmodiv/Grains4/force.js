33dd0d12b1cf464c2b79eb7c8fc8f32568ae29f3
zahrotulfirda <firdauszahrotul@gmail.com>
2018-02-27 10:30:49 +0700

    180227 10.30

---
+++ jsmodiv/Grains4/force.js
@@ -0,0 +1,89 @@
+/*
+	force.js
+	Library of some types of physical force.
+	Sparisoma Viridi | dudung@gmail.com
+	
+	20170214
+	Create this library and define some constants,
+	e.g. kG, kR, kV, kE and some functions
+	grav()	gravitation force,
+	norm()	normal force,
+	coul()	Coulomb force.
+	These two are tested for value only and not yet in
+	simulation.
+	20170215
+	Test the grav() and norm() in a simulation. They worked.
+*/
+
+// Define some constants
+kG = 5E0;
+kR = 1E5;
+kV = 1.0;
+kE = 1.0;
+kV2 = 40.0;
+ks=1.0;
+
+// Class of Force
+function Force() {
+	
+}
+
+// Define gravitation force
+Force.grav = function(p1, p2) {
+	var m1 = p1.m;
+	var m2 = p2.m;
+	var r1 = p1.r;
+	var r2 = p2.r;
+	var r = Vect3.sub(r1, r2);
+	var u = Vect3.uni(r);
+	var f = Vect3.mul(-kG * m1 * m2 / Vect3.dot(r, r), u);
+	return f;
+}
+
+// Define normal force
+Force.norm = function(p1, p2) {
+	var R1 = 0.5 * p1.D;
+	var R2 = 0.5 * p2.D;
+	var r1 = p1.r;
+	var r2 = p2.r;
+	var r = Vect3.sub(r1, r2);
+	var u = Vect3.uni(r);
+	var v1 = p1.v;
+	var v2 = p2.v;
+	var v = Vect3.sub(v1, v2);
+	var xi = Math.max(0, R1 + R2 - Vect3.len(r));
+	var xidot = Vect3.len(v);
+	var f = Vect3.mul(kR * xi - kV * xidot, u);
+	if(xi == 0) {
+		f = new Vect3;
+	}
+	return f;
+}
+
+// Define Coulomb force
+Force.coul = function(p1, p2) {
+	var q1 = p1.q;
+	var q2 = p2.q;
+	var r1 = p1.r;
+	var r2 = p2.r;
+	var r = Vect3.sub(r1, r2);
+	var u = Vect3.uni(r);
+	var f = Vect3.mul(kE * q1 * q2 / Vect3.dot(r, r), u);
+	return f;
+}
+
+// Define viscous force
+Force.visc = function(p) {
+	var v = p.v;
+	var f = Vect3.mul(-kV2, v);
+	return f;
+}
+
+//Define spring force
+Force.spring= function(p1, p2){
+	var r1 = p1.r;
+	var r2 = p2.r;
+	var r = (Vect3.sub(r1, r2));
+	var f = Vect3.mul(-ks,r);
+	return f;
+	}
---
+++ jsmodiv/Grains4/layout.js
@@ -0,0 +1,75 @@
+/*
+	layout.js
+	Define layout for simulation.
+	Sparisoma Viridi | dudung@gmail.com
+	
+	20170215
+	Start this library.
+*/
+
+function layout() {
+	// Create left division
+	var ldiv = document.createElement("div");
+	document.body.appendChild(ldiv);
+	ldiv.style.border = "0px black solid";
+	ldiv.style.height = "402px";
+	ldiv.style.float = "left";
+	ldiv.style.width = "252px";
+	
+	// Create right division
+	var rdiv = document.createElement("div");
+	document.body.appendChild(rdiv);
+	rdiv.style.border = "0px black solid";
+	rdiv.style.height = "402px";
+	rdiv.style.float = "left";
+	
+	// Create text area for input
+	var ta = document.createElement("textarea");
+	ldiv.appendChild(ta);
+	ta.style.color = "black";
+	ta.style.background = "white";
+	ta.rows = "20";
+	ta.cols = "32";
+	ta.style.overflowY = "scroll";
+	ta.style.display = "block";	
+	ta.id = "hout";
+	
+	// Create canvas for drawing
+	var c = document.createElement("canvas");
+	rdiv.appendChild(c);
+	c.id = "canvas";
+	c.style.border = "1px solid #999";
+	c.style.background = "white";
+	c.width = "400";
+	c.height = "400";
+	//c.style.float = "left";
+	var ctx = c.getContext("2d");
+	
+	// Prepare canvas
+	setCanvasCoordinates("canvas");
+	setWorldCoordinates(-8, -8, 8, 8);
+	
+	// Create start button
+	var b1 = document.createElement("button");
+	ldiv.appendChild(b1);
+	b1.innerHTML = "Start";
+	b1.onclick = function() {
+		if(b1.innerHTML == "Start") {
+			b1.innerHTML = "Stop";
+			timer1 = setInterval(run, 1);
+		} else {
+			b1.innerHTML = "Start";
+			clearInterval(timer1);
+		}
+	}
+	
+	// Create save Button
+	var b2 = document.createElement("button");
+	ldiv.appendChild(b2);
+	b2.innerHTML = "Save";
+	b2.onclick = function() {
+		var canvas = document.getElementById("canvas");
+		var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+		window.location.href = image;
+	}
+}
